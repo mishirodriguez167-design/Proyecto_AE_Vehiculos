65ce8dc8396124ddc449a4e7c5a8bb30d0604754
mishirodriguez167-design <mishirodriguez167@gmail.com>
2026-07-05 18:52:40 -0500

    Agregando código del backend main.py

---
+++ Proyecto_AE_Vehiculos/backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="API de Estimación de Vehículos")
+
+# Configuración CORS para permitir conexión con tu Frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Cargar el modelo
+modelo = joblib.load('modelo_vehiculos.pkl')
+
+class DatosVehiculo(BaseModel):
+    marca: str
+    anio: int
+    kilometraje: float
+
+@app.post("/predecir")
+def estimar_precio(vehiculo: DatosVehiculo):
+    datos_diccionario = {
+        'Marca': [vehiculo.marca],
+        'Año': [vehiculo.anio],
+        'Kilometraje': [vehiculo.kilometraje]
+    }
+    df_entrada = pd.DataFrame(datos_diccionario)
+    prediccion = modelo.predict(df_entrada)
+    return {"precio_estimado": float(prediccion[0])}
